ef97ccb77099c385dda97f6f4bd54b1de6d88d74
Alvaro Guimaraes <alvaroguimaraes@gmail.com>
2025-09-02 14:35:37 +0100

    improve nvim editor

--- .config/nvim/lua/config/prelazy.lua
+++ .config/nvim/lua/config/prelazy.lua
@@ -4,3 +4,14 @@ vim.g.loaded_netrwPlugin = 1
 
 -- optionally enable 24-bit colour
 vim.opt.termguicolors = true
+-- set clipboard
+vim.opt.clipboard = "unnamedplus"
+
+vim.opt.cursorline = true
+vim.opt.hidden = true
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.title = true
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
